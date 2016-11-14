93b27575110db3cb5a3f69ea09cb967a584fb81e
Sasha Saulyak <lux1995@yandex.ru>
2016-11-15 01:44:00 +0200

    header

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-  $(".hamburger").click(function(){
-    $(this).toggleClass("is-active");
-  });
+	  $(".hamburger").click(function(){
+	    $(this).toggleClass("is-active");
+
+	  });
+	  $('.header-slider .bxslider').bxSlider({
+pager: false
+		});
+
 });
