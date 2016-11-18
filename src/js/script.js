a87ca7131db3b7f375b99d258af73b473b5de4d5
5tka <ferol2010@yandex.ru>
2016-11-18 17:03:30 +0200

    fixed brands/services ad

--- src/js/script.js
+++ src/js/script.js
@@ -62,7 +62,7 @@ $('.fancybox').fancybox({'width':1100, afterClose: function() {animation=false;}
 
 		$('.brands-wrapper').slick({
 			infinite: true,
-			autoplay: true,
+			// autoplay: true,
 			autoplaySpeed: 2500
 		});
 		$('.gallery__list').slick({
